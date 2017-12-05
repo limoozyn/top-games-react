8180e3a7a939e7b3bf32d4b7b1021026094d2ba2
limoozyn <limoozyn@gmail.com>
2017-12-05 18:14:50 +0200

    Fix the import

--- src/api.js
+++ src/api.js
@@ -1,15 +1,13 @@
-const Twitch = require("twitch.tv-api");
+import Twitch from 'twitch.tv-api';
 const twitch = new Twitch({
   id: process.env.REACT_APP_TWITCH_ID,
   secret: ''
 });
 
-function getTopGames () {
+export function getTopGames () {
   return twitch.getTopGames().then((data) => data.top)
 }
 
-function getGameStreams (game) {
-  return twitch.getTopStreams(game)
+export function getGameStreams (game) {
+  return twitch.getTopStreams({game}).then((data) => data.streams)
 }
-
-module.exports = {getTopGames,getGameStreams}
