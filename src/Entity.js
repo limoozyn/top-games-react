aca55b697cdfed41a9948134cdc1c9ba55ddae4a
limoozyn <limoozyn@gmail.com>
2017-12-18 15:41:37 +0200

    Simplify prop name

--- src/Entity.js
+++ src/Entity.js
@@ -3,12 +3,12 @@ import './Entity.css'
 
 function Game (props) {
   return (
-      <article className={props.entityName}>
-        <figure className={`${props.entityName}__details`}>
-          <img className={`${props.entityName}__picture`} src={props.picture} alt={props.title}/>
+      <article className={props.name}>
+        <figure className={`${props.name}__details`}>
+          <img className={`${props.name}__picture`} src={props.picture} alt={props.title}/>
           <figcaption>
-            <h3 className={`${props.entityName}__title`}>{props.title}</h3>
-            <p className={`${props.entityName}__viewers-count`}>Viewers: {props.viewers}</p>
+            <h3 className={`${props.name}__title`}>{props.title}</h3>
+            <p className={`${props.name}__viewers-count`}>Viewers: {props.viewers}</p>
           </figcaption>
         </figure>
       </article>
