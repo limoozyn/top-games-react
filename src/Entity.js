e92864a6dc7efcc1ea51b33c2982f955b7402f82
limoozyn <limoozyn@gmail.com>
2017-12-18 18:05:14 +0200

    Rename the export/import to meat each other

--- src/Entity.js
+++ src/Entity.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Entity.css'
 
-function Game (props) {
+function Entity (props) {
   return (
-      <article className={props.name}>
-        <figure className={`${props.name}__details`}>
-          <img className={`${props.name}__picture`} src={props.picture} alt={props.title}/>
-          <figcaption>
-            <h3 className={`${props.name}__title`}>{props.title}</h3>
-            <p className={`${props.name}__viewers-count`}>Viewers: {props.viewers}</p>
-          </figcaption>
-        </figure>
-      </article>
-
+    <article className={props.name}>
+      <figure className={`${props.name}__details`}>
+        <img className={`${props.name}__picture`} src={props.picture} alt={props.title}/>
+        <figcaption>
+          <h3 className={`${props.name}__title`}>{props.title}</h3>
+          <p className={`${props.name}__viewers-count`}>Viewers: {props.viewers}</p>
+        </figcaption>
+      </figure>
+    </article>
   )
 }
 
-export default Game
+export default Entity
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Sheet from './App';
+import App from './App';
 
-ReactDOM.render(<Sheet/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
